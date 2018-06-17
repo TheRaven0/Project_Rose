3f59c46d8150d925784e07ca7dac82cda67360d0
TheRaven0 <33192268+TheRaven0@users.noreply.github.com>
2018-06-16 23:51:22 -0500

    +catalogo.java: se agregó la variable string "categoria" que va a ser definida más tarde mediante un intent

--- app/src/main/java/com/example/kevm/project_rose/catalogo.java
+++ app/src/main/java/com/example/kevm/project_rose/catalogo.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class catalogo extends AppCompatActivity {
 
+    String categoria;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
