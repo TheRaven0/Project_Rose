abfca26fc50bf6e34f32e796e7b95415ab92c5f1
TheRaven0 <33192268+TheRaven0@users.noreply.github.com>
2018-06-16 19:58:41 -0500

    +InicioDeSesionActivity.java: agregado onclicklistener y vinculados los botones y edittext con la vista. +RegistroActivity.java: agregado onclicklistener y vinculados los botones y edittext con la vista.

--- app/src/main/java/com/example/kevm/project_rose/InicioDeSesionActivity.java
+++ app/src/main/java/com/example/kevm/project_rose/InicioDeSesionActivity.java
@@ -2,12 +2,39 @@ package com.example.kevm.project_rose;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class InicioDeSesionActivity extends AppCompatActivity {
+public class InicioDeSesionActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button ingresar, registro;
+    private EditText usuario, contraseña;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_de_sesion);
+
+        ingresar = (Button)findViewById(R.id.ingresar);
+        registro = (Button)findViewById(R.id.registro);
+        usuario = (EditText)findViewById(R.id.inicio_username);
+        contraseña = (EditText)findViewById(R.id.inicio_password);
+
+        ingresar.setOnClickListener(this);
+        registro.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ingresar:
+                break;
+            case R.id.registro:
+                break;
+
+                default:
+
+        }
     }
 }
--- app/src/main/java/com/example/kevm/project_rose/RegistroActivity.java
+++ app/src/main/java/com/example/kevm/project_rose/RegistroActivity.java
@@ -2,12 +2,35 @@ package com.example.kevm.project_rose;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class RegistroActivity extends AppCompatActivity {
+public class RegistroActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button registro_conf;
+    private EditText reg_usuario, reg_mail, reg_contraseña;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        registro_conf = (Button)findViewById(R.id.registro_confirmacion);
+        reg_usuario = (EditText)findViewById(R.id.registro_username);
+        reg_mail = (EditText)findViewById(R.id.registro_mail);
+        reg_contraseña = (EditText)findViewById(R.id.registro_password);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.registro_confirmacion){
+
+        }
+        else;
     }
 }
